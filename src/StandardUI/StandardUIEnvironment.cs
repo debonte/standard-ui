e0830a95500992da8f805180998b3c428339d647
Bret Johnson <bret.johnson@microsoft.com>
2020-08-04 01:16:17 -0400

    Add global environment class

---
+++ src/StandardUI/StandardUIEnvironment.cs
@@ -0,0 +1,16 @@
+﻿namespace Microsoft.StandardUI
+{
+    public static class StandardUIEnvironment
+    {
+        private static IStandardUIEnvironment _standardUIEnvironment;
+
+        public static void Init(IStandardUIEnvironment environment)
+        {
+            _standardUIEnvironment = environment;
+        }
+
+        public static IStandardUIEnvironment Environment => _standardUIEnvironment!;
+
+        public static IUIElementFactory UIElementFactory => _standardUIEnvironment.UIElementFactory;
+    }
+}
