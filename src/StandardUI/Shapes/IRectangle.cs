2f44eb2e37ad7411bce70698f1bd55a77a670d2d
Bret Johnson <bret.johnson@microsoft.com>
2020-08-04 01:12:58 -0400

    Update doc

--- src/StandardUI/Shapes/IRectangle.cs
+++ src/StandardUI/Shapes/IRectangle.cs
@@ -3,9 +3,15 @@
     [UIModelObject]
     public interface IRectangle : IShape
     {
+        /// <summary>
+        /// Gets or sets the x-axis radius of the ellipse that is used to round the corners of the rectangle.
+        /// </summary>
         [DefaultValue(0.0)]
         double RadiusX { get; set; }
 
+        /// <summary>
+        /// Gets or sets the y-axis radius of the ellipse that is used to round the corners of the rectangle.
+        /// </summary>
         [DefaultValue(0.0)]
         double RadiusY { get; set; }
     }
