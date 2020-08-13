62203691827e8b0124661f0d4db8a1e9072308d6
Bret Johnson <bret.johnson@microsoft.com>
2020-08-13 03:16:42 -0400

    Add canvas attached properties

---
+++ src/StandardUI/Controls/ICanvasAttached.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.StandardUI.Controls
+{
+    [UIModelObject]
+    public interface ICanvasAttached
+    {
+        [DefaultValue(0.0)]
+        double GetLeft(IUIElement element);
+        void SetLeft(IUIElement element, double length);
+
+        [DefaultValue(0.0)]
+        double GetTop(IUIElement element);
+        void SetTop(IUIElement element, double length);
+    }
+}
