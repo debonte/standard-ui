da47ca32f6741ab4a0551dfda79e65e537b62900
Bret Johnson <bret.johnson@microsoft.com>
2020-08-19 04:07:00 -0400

    Add Canvas extensions for attached props

---
+++ src/StandardUI/Controls/CanvasAttachedExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Microsoft.StandardUI.Controls
+{
+    public static class CanvasAttachedExtensions
+    {
+        public static double GetCanvasLeft(this IUIElement element) => 0;
+        public static void SetCanvasLeft(this IUIElement element, double length) { }
+
+        public static double GetCanvasTop(this IUIElement element) => 0;
+        public static void SetCanvasTop(this IUIElement element, double length) { }
+    }
+}
---
+++ src/StandardUI/Controls/CanvasExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.StandardUI.Shapes;
+
+namespace Microsoft.StandardUI.Controls
+{
+    public static class CanvasExtensions
+    {
+        public static void Add(this ICanvas canvas, double left, double top, IUIElement uiElement)
+        {
+            canvas.Children.Add(uiElement);
+            uiElement.SetCanvasLeft(left);
+            uiElement.SetCanvasTop(top);
+        }
+    }
+}
