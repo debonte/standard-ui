cd0c04983ab6e753b12bcc64a737440045f210ed
Bret Johnson <bret.johnson@microsoft.com>
2020-08-19 04:04:42 -0400

    Add Rect primitive

---
+++ src/StandardUI/Rect.cs
@@ -0,0 +1,22 @@
+﻿namespace Microsoft.StandardUI
+{
+    /// <summary>
+    /// Contains number values that represent the location and size of a rectangle.
+
+    /// </summary>
+    public struct Rect
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Y { get; set; }
+        public double X { get; set; }
+
+        public Rect(double x, double y, double width, double height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+    }
+}
