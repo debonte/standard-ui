5f750b3015ef83f8f31f4bf3cf216095f04e0805
Bret Johnson <bret.johnson@microsoft.com>
2020-08-19 04:07:39 -0400

    Add WpfSampleHost

---
+++ samples/WpfSamplesHost/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.StandardUI.Wpf;
+using System.Windows;
+
+namespace WpfHost
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            WpfStandardUIEnvironment.Init();
+
+            InitializeComponent();
+        }
+    }
+}
