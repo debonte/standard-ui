acd6eae2c52c736584e79831091f444819d8435c
Bret Johnson <bret.johnson@microsoft.com>
2020-08-24 21:36:40 -0400

    Update sample WPF host

--- samples/WpfSamplesHost/MainWindow.xaml.cs
+++ samples/WpfSamplesHost/MainWindow.xaml.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.StandardUI.Wpf;
+﻿using Microcharts;
+using Microsoft.StandardUI;
+using Microsoft.StandardUI.SkiaVisualizer;
+using Microsoft.StandardUI.VisualEnvironment.WpfNative;
+using Microsoft.StandardUI.Wpf;
+using SimpleControls;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace WpfHost
 {
@@ -10,9 +16,55 @@ namespace WpfHost
     {
         public MainWindow()
         {
-            WpfStandardUIEnvironment.Init();
-
+            WpfStandardUIEnvironment.Init(new SkiaVisualEnvironment());
             InitializeComponent();
+
+            /*
+            var radialGauge = new RadialGauge();
+            radialGauge.Width = 100;
+            radialGauge.Height = 100;
+
+            var radialGaugeWpf = new StandardUIUserControlWpf(radialGauge);
+            controlStack.Children.Add(radialGaugeWpf);
+            */
+
+            var barChart = new BarChart()
+            {
+                Entries = CreateChartEntries(),
+                LabelTextSize = 60,
+                LabelOrientation = Microcharts.Orientation.Horizontal,
+                Width = 200,
+                Height = 200
+            };
+            barChart.Build();
+
+            var barChartWpf = new StandardUIUserControlWpf(barChart);
+            controlStack.Children.Add(barChartWpf);
+        }
+
+        public static ChartEntry[] CreateChartEntries()
+        {
+            return new[]
+            {
+                new ChartEntry(200)
+                {
+                        Label = "January",
+                        ValueLabel = "200",
+                        Color = Color.FromHex("#266489")
+                },
+                new ChartEntry(400)
+                {
+                        Label = "February",
+                        ValueLabel = "400",
+                        Color = Color.FromHex("#68B9C0"),
+                },
+                new ChartEntry(100)
+                {
+                        Label = "March",
+                        ValueLabel = "100",
+                        Color = Color.FromHex("#90D585"),
+                },
+            };
         }
     }
 }
