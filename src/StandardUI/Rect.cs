82d32b31eb093da71ce29f121d9bab79fbaba94a
Bret Johnson <bret.johnson@microsoft.com>
2020-08-26 01:56:35 -0400

    Add rect helper

--- src/StandardUI/Rect.cs
+++ src/StandardUI/Rect.cs
@@ -2,7 +2,6 @@
 {
     /// <summary>
     /// Contains number values that represent the location and size of a rectangle.
-
     /// </summary>
     public struct Rect
     {
@@ -10,6 +9,14 @@
         public double Height { get; set; }
         public double Y { get; set; }
         public double X { get; set; }
+
+        public Rect(Point location, Size size)
+        {
+            X = location.X;
+            Y = location.Y;
+            Width = size.Width;
+            Height = size.Height;
+        }
 
         public Rect(double x, double y, double width, double height)
         {
