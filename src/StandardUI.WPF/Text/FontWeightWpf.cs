cfaa27e2c53e2ea0ee899c383c972576b4e3797f
Bret Johnson <bret.johnson@microsoft.com>
2020-09-12 19:24:00 -0400

    Add FontWeight WPF wrapper

---
+++ src/StandardUI.WPF/Text/FontWeightWpf.cs
@@ -0,0 +1,19 @@
+﻿using System.StandardUI.Text;
+
+namespace System.StandardUI.Wpf.Text
+{
+    public struct FontWeightWpf
+    {
+        public static readonly FontWeightWpf Default = new FontWeightWpf(FontWeights.Normal);
+
+        public static FontWeightWpf FromFontWeight(FontWeight fontWeight) => new FontWeightWpf(fontWeight);
+
+        // Auto properties
+        public FontWeight FontWeight { get; }
+
+        public FontWeightWpf(FontWeight fontWeight)
+        {
+            FontWeight = fontWeight;
+        }
+    }
+}
