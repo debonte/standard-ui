3e2c6819f1a16bff83d60492ac1faa4dc6f213f9
Bret Johnson <bret.johnson@microsoft.com>
2021-10-12 11:57:56 -0400

    Rename RadialGauge source file

---
+++ samples/SimpleControls/RadialGaugeImplementation.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.StandardUI;
+using Microsoft.StandardUI.Controls;
+using Microsoft.StandardUI.Media;
+using Microsoft.StandardUI.Shapes;
+using static Microsoft.StandardUI.FactoryStatics;
+
+namespace SimpleControls
+{
+    public interface IRadialGauge : IControl
+    {
+        [DefaultValue(null)]
+        IBrush? Fill { get; set; }
+    }
+
+    public class RadialGaugeImplementation : StandardControlImplementation<IRadialGauge>
+    {
+        public RadialGaugeImplementation(IRadialGauge control) : base(control)
+        { }
+
+        public override IUIElement? Build()
+        {
+            var blueBrush = SolidColorBrush().Color(Colors.Blue);
+
+            return Rectangle() .Width(50) .Height(50) .Stroke(blueBrush) .Fill(Control.Fill);
+        }
+    }
+}
